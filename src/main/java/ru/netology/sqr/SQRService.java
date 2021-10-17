2a4f3b6565806d1cec9a63d483605555b9ed41ca
Денис Гришкевич <cris12@mail.ru>
2021-10-17 19:30:55 +0300

    check first commit

--- src/main/java/ru/netology/sqr/SQRService.java
+++ src/main/java/ru/netology/sqr/SQRService.java
@@ -2,19 +2,19 @@ package ru.netology.sqr;
 
 public class SQRService {
     public int countSquare(int num, int num1) {
-        int iteration_count = 0;
-        {
-            for (int i = 10; i <= 99; i++)
-                if (i * i >= num)
-                    if (i * i <= num1) {
-                        iteration_count++;
-                    }
-        }
+        int iterationcount = 0;
 
-                    return iteration_count;
+        for (int i = 10; i <= 99; i++)
+            if (i * i >= num) {
+                if (i * i <= num1) {
+                    iterationcount++;
                 }
+            }
 
-        }
+        return iterationcount;
+    }
+
+}
 
 
 
--- src/test/java/ru/netology/sqr/SQRServiceTest.java
+++ src/test/java/ru/netology/sqr/SQRServiceTest.java
@@ -6,25 +6,27 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class SQRServiceTest {
 
-        @Test
-        public void shouldCountSquare1() {
-            SQRService sqrService2 = new SQRService();
-            int expected = 5;
-            int actual = sqrService2.countSquare(100, 200);
-            assertEquals(expected, actual);
-        }
-            @Test
-            public void shouldCountSquare2() {
-                SQRService sqrService2 = new SQRService();
-                int expected = 8;
-                int actual = sqrService2.countSquare( 100, 300);
-                assertEquals(expected, actual);
-        }
+    @Test
+    public void shouldCountSquare1() {
+        SQRService sqrService2 = new SQRService();
+        int expected = 5;
+        int actual = sqrService2.countSquare(100, 200);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCountSquare2() {
+        SQRService sqrService2 = new SQRService();
+        int expected = 8;
+        int actual = sqrService2.countSquare(100, 300);
+        assertEquals(expected, actual);
+    }
+
     @Test
     public void shouldCountSquare3() {
         SQRService sqrService2 = new SQRService();
         int expected = 22;
-        int actual = sqrService2.countSquare( 100, 1000);
+        int actual = sqrService2.countSquare(100, 1000);
         assertEquals(expected, actual);
     }
 
@@ -32,14 +34,15 @@ class SQRServiceTest {
     public void shouldCountSquare4() {
         SQRService sqrService2 = new SQRService();
         int expected = 61;
-        int actual = sqrService2.countSquare( 100, 5000);
+        int actual = sqrService2.countSquare(100, 5000);
         assertEquals(expected, actual);
     }
+
     @Test
     public void shouldCountSquare5() {
         SQRService sqrService2 = new SQRService();
         int expected = 1;
-        int actual = sqrService2.countSquare( 0, 100);
+        int actual = sqrService2.countSquare(0, 100);
         assertEquals(expected, actual);
     }
 
@@ -47,7 +50,15 @@ class SQRServiceTest {
     public void shouldCountSquare6() {
         SQRService sqrService2 = new SQRService();
         int expected = 90;
-        int actual = sqrService2.countSquare( 0, 9801);
+        int actual = sqrService2.countSquare(0, 12000);
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void shouldCountSquare7() {
+        SQRService sqrService2 = new SQRService();
+        int expected = 0;
+        int actual = sqrService2.countSquare(0, 0);
+        assertEquals(expected, actual);
     }
+}
